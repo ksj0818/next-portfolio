422a0b62d12fd78ca427e296f5df44a67d9125f9
sjKim <ksj0818@github.com>
2023-08-17 08:25:23 +0900

    이미지 경로 조건부 설정, 포스트 클릭시 링크 이동

--- components/projects/projectItem.js
+++ components/projects/projectItem.js
@@ -1,12 +1,14 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function ProjectItem({ data }) {
   const title = data.properties.이름.title[0].plain_text;
   const tags = data.properties.tag.multi_select;
   const description = data.properties.description.rich_text[0].text.content;
-  const imgSrc = data.cover.external.url;
+  const imgSrc = data.cover.external ? data.cover.external.url : data.cover.file.url;
   const start = data.properties.workdays.date.start;
   const end = data.properties.workdays.date.end;
+  const domain = data.properties.domain.url;
   const calculateDate = (start, end) => {
     const startDateArray = start.split("-");
     const endDateArray = end.split("-");
@@ -21,34 +23,36 @@ export default function ProjectItem({ data }) {
   };
 
   return (
-    <div className="project-card">
-      <Image
-        className="max-h-[200px] rounded-t-xl"
-        src={imgSrc}
-        layout="responsive"
-        width="100%"
-        height="50px"
-        objectFit="cover"
-        quality={100}
-        alt="cover image"
-      />
-      <div className="flex flex-col items-start justify-center px-4 my-1">
-        <h1 className="my-2 text-lg font-bold sm:text-2xl md:text-3xl">{title}</h1>
-        <h2 className="text-lg">{description}</h2>
-        <h3 className="text-sm">
-          작업 기간 : {`${start}~${end}`}({calculateDate(start, end)}일)
-        </h3>
-        <div className="flex my-1 gap-x-2">
-          {tags.map((tag) => (
-            <p
-              key={tag.id}
-              className={`rounded-md text-slate-700/90 bg-sky-400 dark:bg-sky-700 dark:text-white/90 px-1`}
-            >
-              {tag.name}
-            </p>
-          ))}
+    <Link href={domain}>
+      <div className="cursor-pointer project-card">
+        <Image
+          className="max-h-[200px] rounded-t-xl"
+          src={imgSrc}
+          layout="responsive"
+          width="100%"
+          height="50px"
+          objectFit="cover"
+          quality={100}
+          alt="cover image"
+        />
+        <div className="flex flex-col items-start justify-center px-4 my-1">
+          <h1 className="my-2 text-lg font-bold sm:text-2xl md:text-3xl">{title}</h1>
+          <h2 className="text-lg">{description}</h2>
+          <h3 className="text-sm">
+            작업 기간 : {`${start}~${end}`}({calculateDate(start, end)}일)
+          </h3>
+          <div className="flex my-1 truncate gap-x-2">
+            {tags.map((tag) => (
+              <p
+                key={tag.id}
+                className={`rounded-md text-slate-700/90 bg-sky-400 truncate dark:bg-sky-700 dark:text-white/90 px-1`}
+              >
+                {tag.name}
+              </p>
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
